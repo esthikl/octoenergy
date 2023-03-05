dbb4c6d062d849cb6f0a4c9f16a1cccf87d9a84b
Esther Kleinhenz <esther.kleinhenz@p-perior.com>
2023-03-05 21:27:41 +0100

    #5 added increaseQuantity

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -1,15 +1,19 @@
-export default function Header() {
+export const Header = (count: any) => {
+
     return (
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Octopus Energy</span>
-            <img className="h-8 w-auto" src="octopus-logo.svg" alt="Octopus Energy Logo" />
-          </a>
-        </div>
-        <div className="lg:flex lg:flex-1 lg:justify-end">
-          <img className="h-8 w-auto" src="basket.svg" alt="Basekt" />
-        </div>
-      </nav>
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Octopus Energy</span>
+                    <img className="h-8 w-auto" src="octopus-logo.svg" alt="Octopus Energy Logo" />
+                </a>
+            </div>
+            <div className="lg:flex lg:flex-1 lg:justify-end">
+                <span>{count}
+                </span>
+                <img className="h-8 w-auto" src="basket.svg" alt="Basekt" />
+            </div>
+        </nav>
     )
-  }
+}
+export default Header
--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -1,9 +1,29 @@
+import React from "react";
 import Header from "../components/header";
 
 export default function Product() {
+
+  const [count, setCount] = React.useState(0);
+
+  function increaseQuantity() {
+    setCount(count + 1)
+  }
+
   return (
     <div>
-      <Header></Header>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Octopus Energy</span>
+            <img className="h-8 w-auto" src="octopus-logo.svg" alt="Octopus Energy Logo" />
+          </a>
+        </div>
+        <div className="lg:flex lg:flex-1 lg:justify-end">
+          <span>{count}
+          </span>
+          <img className="h-8 w-auto" src="basket.svg" alt="Basekt" />
+        </div>
+      </nav>
       <div className="flex justify-items-center">
         <div className="w-full max-w-lg rounded-lg shadow">
           <img className="p-8 rounded-3xl" src="philips-plumen.jpg" alt="Pilips Plumen" />
@@ -18,7 +38,7 @@ export default function Product() {
                   <button className="bg-plum text-gray-600 hover:text-gray-700 hover:bg-plum h-full w-20 rounded-l cursor-pointer outline-none">
                     <span className="m-auto text-2xl font-thin">−</span>
                   </button>
-                  <div className="outline-none focus:outline-none text-center w-full bg-transparent font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"></div>
+                  <div className="outline-none focus:outline-none text-center w-full bg-transparent font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none" title="Current quantity">{count}</div>
                   <button className="bg-sohoLights text-gray-600 hover:text-gray-700 hover:bg-plum h-full w-20 rounded-r cursor-pointer">
                     <span className="m-auto text-2xl font-thin">+</span>
                   </button>
@@ -26,7 +46,7 @@ export default function Product() {
               </div>
             </div>
             <div className="flex pt-4 lg:pt-6">
-              <a href="#" className="w-full bg-sohoLights text-white hover:bg-plum focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
+              <button onClick={increaseQuantity} className="w-full bg-sohoLights text-white hover:bg-plum font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</button>
             </div>
           </div>
         </div>
